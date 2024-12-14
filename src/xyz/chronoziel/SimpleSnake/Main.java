d396a05370b44518ac0e227f190c1ea6ce422ef0
Mahdi Chowdhury (Kashieru1987) <mahdichowdhury07@gmail.com>
2024-12-14 01:58:45 +0000

    Created Main stub

---
+++ src/xyz/chronoziel/SimpleSnake/Main.java
@@ -0,0 +1,23 @@
+package xyz.chronoziel.SimpleSnake;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	public Main() {
+		SwingUtilities.invokeLater(() -> {
+			try {
+
+				//time to do the coding funny
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
