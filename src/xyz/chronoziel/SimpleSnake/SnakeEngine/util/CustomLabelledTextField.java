e433866199636e803d17941423a30dc7a5f9a23f
Mahdi Chowdhury (Kashieru1987) <mahdichowdhury07@gmail.com>
2024-12-26 00:36:13 +0000

    Created Custom Labelled Text Field

---
+++ src/xyz/chronoziel/SimpleSnake/SnakeEngine/util/CustomLabelledTextField.java
@@ -0,0 +1,10 @@
+package xyz.chronoziel.SimpleSnake.SnakeEngine.util;
+
+import java.awt.Container;
+
+public class CustomLabelledTextField extends Container {
+
+	public CustomLabelledTextField() {
+	}
+
+}
